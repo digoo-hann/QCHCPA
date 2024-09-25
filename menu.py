33a255bc0b206b4fed89f1cc8ffb453a0b6ecbbb
digoo-hann <rodieh2@gmail.com>
2024-09-25 10:06:41 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-
-st.sidebar.page_link("pages/bi_data.py", label="Análise de Dados do BI")
-st.sidebar.page_link("pages/cq_management.py", label="Gerência de Controle de Qualidade")
+def menu():
+        st.sidebar.page_link("pages/bi_data.py", label="Análise de Dados do BI")
+        st.sidebar.page_link("pages/cq_management.py", label="Gerência de Controle de Qualidade")
         
 
 
